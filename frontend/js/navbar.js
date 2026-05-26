d652376d33c063e3b4ef1a1173568be077eb3ecf
miks <mcdeguzman12@up.edu.ph>
2026-05-27 06:04:11 +0800

    CSS things

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -1,10 +1,12 @@
-fetch("/navbar.html")
+fetch("navbar.html")
+//fetch("/navbar.html")
     .then(res => res.text())
     .then(data => {
         document.getElementById("navbar").innerHTML = data;
     });
 
-fetch("/footer.html")
+fetch("footer.html")
+//fetch("/footer.html")
     .then(res => res.text())
     .then(data => {
         document.getElementById("footer").innerHTML = data;
