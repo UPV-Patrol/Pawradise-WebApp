4c668e0fdc7164d94410a1f709404201d703b1f9
rskaindoy <rskaindoy@up.edu.ph>
2026-05-28 06:36:36 +0800

    deploy

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -2,12 +2,16 @@ const mysql = require('mysql2');
 
 //connection
 const pool = mysql.createPool ({
-    host: 'localhost',       
-    user: 'root',            
-    password: '',         
-    database: 'upv_pawtrol',  
+    host: process.env.DB_HOST,          // host: 'localhost',       
+    user: process.env.DB_USER,          // user: 'root',            
+    password: process.env.DB_PASSWORD,  // password: '',         
+    database: process.env.DB_NAME,      // database: 'upv_pawtrol',  
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true
+    },
     waitForConnections: true,
-    connectionLimit: 10,   // accomm only 10 people at the same time
+    connectionLimit: 10,                // accomm only 10 people at the same time
     queueLimit: 0
 });
 
